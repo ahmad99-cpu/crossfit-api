650908abeed48495d2d79dae4e6d6103fbb2b38e
Mohammad-Ismail-Dev <mohamad.ismail.dev@gmail.com>
2023-12-12 11:50:44 +0200

    keef msh 3m tntibih lal t??

--- routes/api.php
+++ routes/api.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/gym-classes', GymClassController::class);
 Route::apiResource('/products', ProductController::class);
 Route::apiResource('/members', MemberController::class);
-Route::apiResource('/instructors', InsructorController::class);
+Route::apiResource('/instructors', InstructorController::class);
 Route::apiResource('/personal-trainers', PersonalTrainerController::class);
 Route::apiResource('/sessions', SessionController::class);
 Route::apiResource('/member-classes', MemberClassController::class);
