2cc69fff6a603912a1ef2f892a7b5a7930ca65e3
Mohammad-Ismail-Dev <mohamad.ismail.dev@gmail.com>
2023-12-21 15:49:27 +0200

    made period integer

--- app/Http/Requests/StoresessionRequest.php
+++ app/Http/Requests/StoresessionRequest.php
@@ -25,7 +25,7 @@ class StoreSessionRequest extends FormRequest
 	{
 		return [
 			'day' => 'required|string',
-			'period' => 'required|string',
+			'period' => 'required|integer',
 			'instructor_id' => 'required|exists:instructors,id',
 			'gym_class_id' => 'required|exists:gym_classes,id',
 		];
