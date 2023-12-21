c0c4d6ec8bd48e7ef6b41732da9e1e32255b2d34
ahmad <ahmad.hammoud.1999@hotmail.com>
2023-12-21 14:43:55 +0200

    MemberSessions

---
+++ app/Http/Controllers/Api/MemberSessionController.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\MemberSessionResource;
+use App\Models\MemberSession;
+use App\Http\Requests\StoreMemberSessionRequest;
+use App\Http\Requests\UpdateMemberSessionRequest;
+
+class MemberSessionController extends Controller
+{
+
+	public function index()
+	{
+		return MemberSessionResource::collection(MemberSession::all());
+	}
+
+
+
+	public function store(StoreMemberSessionRequest $request)
+	{
+		$memberSession = MemberSession::create($request->validated());
+		return MemberSessionResource::make($memberSession);
+	}
+
+
+	public function show(MemberSession $memberSession)
+	{
+		return MemberSessionResource::make($memberSession);
+	}
+
+
+
+
+	public function update(UpdateMemberSessionRequest $request, MemberSession $memberSession)
+	{
+		$memberSession->update($request->validated());
+		return MemberSessionResource::make($memberSession);
+	}
+
+
+	public function destroy(MemberSession $memberSession)
+	{
+		$memberSession->delete();
+		return response()->noContent();
+	}
+}
